e0f6f5beadde940a2d215b221f4b138be83f5e7d
John Ho <john.ho@mountaingapsolutions.com>
2019-03-16 19:06:21 -0400

    HVU-12 #comment Add HSTS back in. And install zip.

--- src/productionServer.js
+++ src/productionServer.js
@@ -9,11 +9,29 @@ const path = require('path');
 const hsts = require('hsts');
 const {api, validate} = require('./restServiceMethods');
 
+const useHsts = process.env.USE_HSTS !== null && process.env.USE_HSTS !== undefined ? !!process.env.USE_HSTS && process.env.USE_HSTS !== 'false' : true;
 console.log(`Starting server on port ${chalk.yellow(port)}...`);
 
 const noCacheUrls = ['/'];
 express().use(compression())
     .disable('x-powered-by')
+    .use((req, res, next) => {
+        // Debugging snippet to figure out exactly what the proxy request header contains.
+        // const logObject = {
+        //     headerData: JSON.parse(JSON.stringify(req.headers)),
+        //     message: `Request made for: ${req.headers.host}${req.url}`,
+        //     useHsts: useHsts,
+        //     reqSecure: req.secure,
+        //     reqProtocol: req.protocol
+        // };
+        // console.log(logObject);
+
+        // If requesting http, forward to https.
+        if (useHsts && req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(307, `https://${req.headers.host}${req.url}`);
+        }
+        return next();
+    })
     .use(hsts({
         force: true,
         preload: true
