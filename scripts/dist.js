9f552906067feae7c8ffc55bffd489e9a5c982ac
John Ho <john.ho@mountaingapsolutions.com>
2019-03-16 19:20:56 -0400

    HVU-12 #comment Use archiver to zip instead of trying from Bitbucket.

---
+++ scripts/dist.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+
+const archive = require('archiver')('zip');
+const chalk = require('chalk');
+const fs = require('fs');
+
+const ZIP_FILE_NAME = 'vault-web-ui.zip';
+
+archive.pipe(fs.createWriteStream(ZIP_FILE_NAME));
+archive.directory('dist/', false);
+archive.finalize();
+
+console.log(`\r\n${chalk.bold.yellow.underline('Successful build - congratulations!')}\r\n`);
