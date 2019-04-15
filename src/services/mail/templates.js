3b90ab2a7e0a380d48bea2e0e1fb20a5cf89f7fe
Jay Ramirez <jay.ramirez@mountaingapsolutions.com>
2019-04-15 14:51:15 -0400

    HVU-69 #comment Now using the new getUser from userService.

--- src/services/mail/templates.js
+++ src/services/mail/templates.js
@@ -6,13 +6,18 @@
  * @returns {Object}
  */
 const getRequestEmailContent = (emailData) => {
-    const {requesterEntityId, customSubject, domain, requestData} = emailData;
+    const {requester, customSubject, domain, requestData} = emailData;
+    const {id: requesterEntityId} = requester;
+    const {email, firstName, lastName} = requester.metadata;
     const subject = customSubject || 'Request Access';
     const body = `<html>
         <body>
             <p>Requesting access to: ${requestData}</p>
             <p>Domain: ${domain}</p>
-            <p>Requester Entity ID: ${requesterEntityId}</p>
+            <p>Requester</p>
+            <p>Entity ID: ${requesterEntityId}</p>
+            <p>Name: ${firstName} ${lastName}</p>
+            <p>Email: ${email}</p>
         </body>
     </html>`;
     const content = {
@@ -29,13 +34,19 @@ const getRequestEmailContent = (emailData) => {
  * @returns {Object}
  */
 const getApprovalEmailContent = (emailData) => {
-    const {approver, requesterEntityId, customSubject, domain, requestData, status} = emailData;
+    const {approver, requester, customSubject, domain, requestData, status} = emailData;
+    const {id: requesterEntityId} = requester;
+    const {email, firstName, lastName} = requester.metadata;
     const subject = customSubject || 'Request Access Approval Status';
     const body = `<html>
         <body>
             <p>Requesting access to: ${requestData}</p>
             <p>Domain: ${domain}</p>
-            <p>Requester Entity ID: ${requesterEntityId}</p>
+            <p>Requester</p>
+            <p>Entity ID: ${requesterEntityId}</p>
+            <p>Name: ${firstName} ${lastName}</p>
+            <p>Email: ${email}</p>
+            <br/>
             <p>Status: ${status}</p>
             <p>Updated By: ${approver}</p>
         </body>
