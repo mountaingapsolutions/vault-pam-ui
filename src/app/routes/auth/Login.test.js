eefba6a0d538a60071dcab18886ffd9ccc92a3c9
Jay Ramirez <jay.ramirez@mountaingapsolutions.com>
2019-06-10 11:02:34 -0400

    HVU-141 Login tests.

--- src/app/routes/auth/Login.test.js
+++ src/app/routes/auth/Login.test.js
@@ -15,6 +15,7 @@ import {Router} from 'react-router-dom';
 
 import Login from './Login';
 import reduxThunk from 'redux-thunk';
+import {Button, Tab, TextField} from '@material-ui/core';
 
 jest.mock('app/core/actions/sessionAction');
 jest.mock('app/core/actions/systemAction');
@@ -42,10 +43,11 @@ const _configureStore = (initialState) => {
  * Renders an instance of Login for testing.
  *
  * @private
+ * @param {Object} [initialState] = The initial state.
  * @returns {ReactElement}
  */
-const _getInstance = () => {
-    return <Provider store={_configureStore()}>
+const _getInstance = (initialState) => {
+    return <Provider store={_configureStore(initialState)}>
         <Router history={createMemoryHistory()}>
             <Login/>
         </Router>
@@ -58,7 +60,7 @@ afterAll(() => {
 
 beforeEach(() => {
     jest.clearAllMocks();
-
+    sessionAction.login.mockImplementation(() => () => new Promise((resolve) => resolve({})));
     systemAction.getConfig.mockImplementation(() => () => new Promise((resolve) => resolve({})));
 });
 
@@ -66,21 +68,63 @@ it('renders correctly', () => {
     expect(renderer.create(_getInstance()).toJSON()).toMatchSnapshot();
 });
 
-it('sets the response token if authenticated', () => {
-    sessionAction.login.mockImplementation(() => () => new Promise((resolve) => resolve({
-        data: {
-            data: {
-                id: 'foobar'
-            }
-        }
-    })));
-    // TODO - Need to force _onSubmit to be clicked to trigger authenticate.
-    renderer.create(_getInstance());
-    // Returning a promise with delay to ensure validateToken is executed first.
-    // return new Promise((resolve) => {
-    //     setTimeout(() => {
-    //         expect(sessionAction.setToken).toHaveBeenCalledTimes(1);
-    //         resolve();
-    //     }, 0);
-    // });
+
+it('toggles the tab menu if tab is clicked', () => {
+    const testInstance = renderer.create(_getInstance()).root;
+
+    expect(testInstance.findAllByType(Tab)[0].props.selected).toBe(true);
+    // click userpass tab
+    testInstance.findAllByType(Tab)[1].props.onChange(null, 1);
+
+    expect(testInstance.findAllByType(Tab)[0].props.selected).toBe(false);
+    expect(testInstance.findAllByType(Tab)[1].props.selected).toBe(true);
+});
+
+
+it('check if the authentication failed', () => {
+    const testInstance = renderer.create(_getInstance()).root;
+
+    testInstance.findAllByType(Button)[1].props.onClick({
+        preventDefault: jest.fn()
+    });
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            expect(sessionAction.login).toHaveBeenCalledTimes(0);
+            resolve();
+        }, 0);
+    });
+});
+
+it('check if the authentication was successful', () => {
+    const testInstance = renderer.create(_getInstance()).root;
+
+    // click the userpass tab
+    testInstance.findAllByType(Tab)[1].props.onChange(null, 1);
+    expect(testInstance.findAllByType(Tab)[1].props.selected).toBe(true);
+
+    //  populate the username/password input fields
+    const textfieldInstance = testInstance.findAllByType(TextField);
+    const usernameProps = textfieldInstance[0].props;
+    const passwordProps = textfieldInstance[1].props;
+    usernameProps.onChange({
+        preventDefault: jest.fn(),
+        target: {name: 'username', value: 'foo'}
+    });
+    passwordProps.onChange({
+        preventDefault: jest.fn(),
+        target: {name: 'password', value: 'bar'}
+    });
+
+    // click the login button
+    const buttonProps = testInstance.findAllByType(Button)[1].props;
+    buttonProps.onClick({
+        preventDefault: jest.fn()
+    });
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            expect(sessionAction.login).toHaveBeenCalledTimes(1);
+            resolve();
+        }, 0);
+    });
 });
